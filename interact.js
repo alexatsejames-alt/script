69cf06b9733da35435f189b79e591bad2d1e71ca
alexatsejames-alt <alexatsejames@gmail.com>
2026-04-17 01:46:27 -1200

    first name

---
+++ interact.js
@@ -0,0 +1,30 @@
+
+
+let firstName = "alex";
+let lastName = "james";
+
+let fullName = lastName + " " + firstName;
+console.log(fullName);
+console.log (typeof firstName)
+
+
+ let firstName = "Alexxy";
+ let lastName = "james";
+ console.log(`my name is ${firstName} ${lastName}`);
+
+ let me = "hello world"/8;
+ console.log(me)
+ console.log(typeof me )
+
+ let result = 5 + '10';
+ console.log(result);
+ console.log(typeof result);
+
+ let sub = '2' * 5;
+ console.log(sub);
+ console.log(typeof sub)
+
+ let mu
+
+
+
---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Hi there!");
+console.log("I am excited talk to to you.");
+let bot  ;
+bot = "Alex";
+let botLocation = "the universe";
+console.log("Allow me to introduce my self.");
+let botIntroduction = ("My name is "+ bot + ".");
+console.log(botIntroduction);
+let botLocationSentence = ("I live in " + botLocation +".");
+console.log(botLocationSentence);
+bot ="alexxy";
+let nicknameIntroduction =("my nickname is " + bot + ".");
+console.log(nicknameIntroduction);
+bot = "awesomeTeacherBot";
+let newLicknameGreeting = ("i love my nickname but i wish people would call me " + bot + ".");
+console.log(newLicknameGreeting);
+let favoriteSubject = "Computer Science";
+let favoriteSubjectSentence = ("My favorite subject is " + favoriteSubject + ".");
+console.log(favoriteSubjectSentence);
+console.log("It was nice to talk to you have a nice day!");
+
+
+
